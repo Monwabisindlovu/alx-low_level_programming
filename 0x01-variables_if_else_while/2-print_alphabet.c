0c649bf8fe35849bfbf210a60b99ec5ce0a87b73
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-17 02:53:42 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,23 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
+char letter = 'a';
+
+while (letter <= 'z')
 {
-    char letter = 'a';
+putchar(letter);
+letter++;
 
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
 
-    putchar('\n');
+putchar('\n');
 
-    return 0;
+return (0);
 }
-The program initializes a variable letter with the lowercase letter 'a', and then uses a while loop to print the letters from 'a' to 'z' using the putchar function. After the loop, it prints a newline character using putchar as well.
-
-Note that the code only uses the putchar function, as required by the prompt, and it uses it twice (once to print each letter, and once to print the newline character).
-
 
 
 
