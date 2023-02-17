00db6be5502966d7c8220ff0d2f380dc0888c0eb
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-17 05:51:33 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        if (letter != 'e' && letter != 'q')
+        {
+            putchar(letter);
+        }
+
+        letter++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
