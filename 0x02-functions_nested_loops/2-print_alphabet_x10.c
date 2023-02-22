8f947cf2e30e534b3250132fb47e5b4c390562db
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-21 20:31:14 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* print_alphabet - prints the alphabet in lowercase
+*/
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+}
