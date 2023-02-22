943c71fbe1ff3db92ba538d39d9d501218e081a5
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-22 11:21:49 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
-#include "main.h"
 #include <unistd.h>
+
 /**
- * _putchar - writes the charecter c to stdout
- * @C: The charecter to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  * 
- * Return: 0n success l.
- * 0n error, -l is returned, and errno is set appropriately.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-return (write(l, &c, l));
+ return (write(1, &c, 1));
 }  
