1ff3df1a494624e68f5312585c4a5ab26cdf39e4
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-21 20:46:50 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,20 @@
 /**
 * print_alphabet - prints the alphabet in lowercase
 */
-void print_alphabet(void)
-{
-char c;
+void print_alphabet_x10(void);
 
-for (c = 'a'; c <= 'z'; c++)
+int main(void)
+{
+print_alphabet_x10();
+return 0;
+}
+
+void print_alphabet_x10(void)
+{
+for (int i = 0; i < 10; i++) {
+for (char c = 'a'; c <= 'z'; c++) {
 putchar(c);
+}
 putchar('\n');
+}
 }
