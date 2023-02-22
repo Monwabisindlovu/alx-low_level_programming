49a1d1e3f134f655dbbfec31a21f0d3cc40e0505
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-22 11:52:16 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,15 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * 
- * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise.
+ * @c: character to check
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
-int _isalpha(int c)
 
+int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
-else
+
 return (0);
 }
