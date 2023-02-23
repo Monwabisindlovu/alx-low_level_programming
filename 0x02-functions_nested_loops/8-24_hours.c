ffcff7cd8a733a8bb3fee273f907e4e5e09045eb
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-22 20:33:09 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
@@ -6,27 +8,13 @@
  */
 void jack_bauer(void)
 {
-int h1, h2, m1, m2;
+int hour, minute;
 
-for (h1 = 0; h1 < 3; h1++)
+for (hour = 0; hour < 24; hour++)
 {
-for (h2 = 0; h2 < 4; h2++)
+for (minute = 0; minute < 60; minute++)
 {
-for (m1 = 0; m1 < 6; m1++)
-{
-for (m2 = 0; m2 < 10; m2++)
-{
-if (h1 < 2 || (h1 == 2 && h2 < 4))
-{
-_putchar(h1 + '0');
-_putchar(h2 + '0');
-_putchar(':');
-_putchar(m1 + '0');
-_putchar(m2 + '0');
-_putchar('\n');
-}
-}
-}
+printf("%02d:%02d\n", hour, minute);
 }
 }
 }
