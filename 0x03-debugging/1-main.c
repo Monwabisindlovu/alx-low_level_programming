e27971c64dafe131ad4f89b5a81d0fbe42dbbb47
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-24 02:44:36 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-while (i < 10)
-{
-/* putchar(i); Commenting out this line to avoid infinite loop */
-i++;
-}
+/* Commenting out the infinite loop */
+/* while (i < 10) */
+/* { */
+/*         putchar(i); */
+/* } */
 
 printf("Infinite loop avoided! \\o/\n");
 
