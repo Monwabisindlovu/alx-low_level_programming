5b326a8d63db94d97c76181d4c76eea6287b10f4
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-24 04:12:57 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-*print_sign - prints the sign of a number
-*@n: the number to check
-*Return: 1 if the number is positive 
-*0 if it is zero, and -1 if it is negative
-*/
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 and prints + if n is greater than zero
+ *         0 and prints 0 if n is zero
+ *        -1 and prints - if n is less than zero
+ */
 int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+_putchar('+');
 return (1);
 }
 else if (n == 0)
 {
-putchar('0');
+_putchar('0');
 return (0);
 }
 else
 {
-putchar('-');
+_putchar('-');
 return (-1);
 }
 }
