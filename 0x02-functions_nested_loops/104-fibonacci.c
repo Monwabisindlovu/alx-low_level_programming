d94490f92c21e5a29f0817d14f54e331d9e05e51
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-24 04:45:33 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,17 +11,12 @@ int main(void)
 int i;
 unsigned long int a = 1, b = 2, c;
 
-printf("%lu, %lu, ", a, b);
+printf("%lu, %lu", a, b);
 
 for (i = 3; i <= 98; i++)
 {
 c = a + b;
-printf("%lu", c);
-
-if (i != 98)
-{
-printf(", ");
-}
+printf(", %lu", c);
 
 a = b;
 b = c;
