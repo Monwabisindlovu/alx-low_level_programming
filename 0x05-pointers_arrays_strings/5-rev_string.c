ea503d0e5a1a1dc2e2be36dbe8c62d4bbb60bf65
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-26 23:19:26 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: String to reverse
+ */
+ 
+void rev_string(char *s)
+{
+int length = 0;
+char temp;
+int i;
+
+/* Find the length of the string */
+while (*(s + length) != '\0')
+{
+length++;
+}
+
+/* Swap characters from start and end of the string */
+for (i = 0; i < length / 2; i++)
+{
+temp = *(s + i);
+*(s + i) = *(s + length - i - 1);
+*(s + length - i - 1) = temp;
+}
+}
