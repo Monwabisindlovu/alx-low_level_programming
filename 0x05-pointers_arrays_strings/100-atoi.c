ed5eb3deaed7d0e67bd53f93dc9ef6eb8d768fa7
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-02-27 02:22:56 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer
- * @s: pointer to the string
+ * _atoi - converts a string to an integer.
  *
- * Return: the converted integer
+ * @s: pointer to the string to convert.
+ *
+ * Return: the converted integer value.
  */
 int _atoi(char *s)
 {
-int sign = 1, num = 0, i = 0;
+int sign = 1;
+unsigned int num = 0;
+int i = 0;
 
 while (s[i])
 {
 if (s[i] == '-')
 sign *= -1;
 if (s[i] >= '0' && s[i] <= '9')
-num = num * 10 + s[i] - '0';
+num = num * 10 + (s[i] - '0');
 if (num != 0 && !(s[i] >= '0' && s[i] <= '9'))
 break;
 i++;
 }
+
 return (num * sign);
 }
