129fbfb8da4f0b0af79e84f1be941a5eee546cd8
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-03-02 02:55:43 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+
+char *leet(char *str)
+{
+int i, j;
+char leet[] = "aAeEoOtTlL44337071";
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (str[i] == leet[j])
+{
+str[i] = leet[j + 10];
+break;
+}
+}
+}
+
+return (str);
+}
+
