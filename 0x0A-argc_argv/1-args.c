c2cb7d6e29aba75d25db56d037ae97f845b16625
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-03-10 14:07:22 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * my_func - an example function with an unused parameter
- * @unused_param: a parameter that is not used in the function body
+ * main - entry point for the program
  *
- * Return: none
- */
-int main(int argc, char *argv[] __attribute__((unused)))
-{
-/* do something else */
-}
-
-/**
- * main - prints the number of arguments passed into it
- * @argc: the number of command-line arguments
- * @argv: an array of strings containing the command-line arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 on success
+ * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-int __attribute__((unused)) x = 10;
-int y = 20;
+(void) argv; /* Ignore argv */
 
 printf("%d\n", argc - 1);
-my_func(y);
 
 return (0);
 }
