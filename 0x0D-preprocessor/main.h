632f5af1f26e956fae587786123fdac803bedb8a
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-03-20 05:08:39 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+#define UNUSED(x) (void)(x)
+
+/* Function prototypes */
+int add(int a, int b);
+char *reverse(char *s);
+
+#endif 
