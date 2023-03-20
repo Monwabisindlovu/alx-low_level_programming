5b91ffa7cfa5cbbefd1c35ff60d6ed89ec043312
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-03-20 05:25:16 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of the file it was compiled from, followed by a new line
+ * main - Prints the name of the file it was compiled from,
+ * followed by a new line
  *
  * Return: Always 0.
  */
