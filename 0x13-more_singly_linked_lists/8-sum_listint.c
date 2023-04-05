bdeb9a0f02fcee1a61f5eb56575960fef4cbb2af
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-04-05 10:58:11 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,12 +6,14 @@
  *
  * Return: the sum of all the data (n) in the nodes, or 0 if the list is empty
  */
+
 int sum_listint(listint_t *head)
 {
 int sum = 0;
 listint_t *node = head;
 
-while (node != NULL) {
+while (node != NULL)
+{
 sum += node->n;
 node = node->next;}
 
