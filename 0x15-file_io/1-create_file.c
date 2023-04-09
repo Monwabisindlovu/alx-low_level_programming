ce36bea4f95b027bb25dd7cecb2e0158055e6b81
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-04-09 02:14:43 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 /**
- * create_file - creates a file with specified permissions and writes a NULL
- * terminated string to it
- * @filename: name of file to create
- * @text_content: NULL terminated string to write to file
+ * create_file - Creates a file and writes text to it
+ * @filename: Name of the file to create
+ * @text_content: Text to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
+int fd, write_count;
 
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 return (-1);
 
 if (text_content != NULL)
-{
-while (*text_content != '\0')
-text_content++;
-write(fd, text_content, text_content - text_content);
-}
+write_count = write(fd, text_content, strlen(text_content));
+else
+write_count = 0;
 
 close(fd);
+
+if (write_count == -1)
+return (-1);
+
 return (1);
 }
