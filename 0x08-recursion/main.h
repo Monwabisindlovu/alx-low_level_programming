3626114046e93b61eac52fb0e1a79baf15185318
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-07 19:21:55 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int putchar(int c);
+int _putchar(char c);
+
+/* Function prototypes */
+void function1(void);
+int function2(int a, int b);
+float function3(float x, float y);
+void function4(char* str);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -11,5 +18,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /* MAIN_H */
 
