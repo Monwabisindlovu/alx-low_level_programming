1296936a1324a2d56b4b78b9a33f67b275b61723
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-09 05:23:16 -0700

    0-whatsmyname.c

---
+++ 0x0A. C - argc, argv/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int is_positive_number(char *str);
+int main(int argc, char *argv[]);
+int get_minimum_coins(int cents);
+int main(int argc, char *argv[]);
+int _putchar(char c);
+#endif /* MAIN_H */
+
