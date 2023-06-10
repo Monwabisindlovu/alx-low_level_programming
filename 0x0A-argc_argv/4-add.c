92551a0648afcf724408f58590609f363d911f3f
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-10 11:54:45 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_positive_number - Check if a string is a positive number
+ * @str: The string to check
+ *
+ * Return: 1 if the string is a positive number, 0 otherwise
+ */
+int is_positive_number(char *str)
+{
+int i = 0;
+
+if (str[i] == '-')
+return (0);
+
+while (str[i])
+{
+if (!isdigit(str[i]))
+return (0);
+i++;
+}
+
+return (1);
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+int i, sum = 0;
+
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+
+for (i = 1; i < argc; i++)
+{
+if (!is_positive_number(argv[i]))
+{
+printf("Error\n");
+return (1);
+}
+
+sum += atoi(argv[i]);
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
+
