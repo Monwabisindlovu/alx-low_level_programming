d0bb52a445a6c9835c787cb6ec3b5a17ed5dfbe2
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-10 11:23:46 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+(void)argc; /* Unused parameter */
+
+printf("%s\n", argv[0]);
+
+return 0;
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+void print_name(char *name);
+void print_arguments_count(int argc);
+void print_arguments(char *argv[]);
+int multiply(int a, int b);
+int add_positive_numbers(int argc, char *argv[]);
+int minimum_coins(int cents);
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
