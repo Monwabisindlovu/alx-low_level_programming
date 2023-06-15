602aea2cfe31f9557e5a7e81e104527a82ae089f
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-15 07:12:56 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,4 @@
-i#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -19,8 +19,8 @@ len1++;
 while (num2[len2])
 len2++;
 
-result = calloc(len1 + len2, sizeof(int));
 
+result = calloc(len1 + len2, sizeof(int));
 
 for (i = len1 - 1; i >= 0; i--)
 {
@@ -29,6 +29,7 @@ for (j = len2 - 1; j >= 0; j--)
 int digit1 = num1[i] - '0';
 int digit2 = num2[j] - '0';
 int product = digit1 * digit2;
+
 
 int sum = result[i + j + 1] + product;
 result[i + j + 1] = sum % 10;
@@ -40,8 +41,6 @@ result[i + j] += sum / 10;
 i = 0;
 while (result[i] == 0 && i < len1 + len2 - 1)
 i++;
-
-
 for (; i < len1 + len2; i++)
 printf("%d", result[i]);
 printf("\n");
@@ -62,13 +61,11 @@ int main(int argc, char *argv[])
 {
 int i, j;
 
-
 if (argc != 3)
 {
 printf("Error\n");
 return (98);
 }
-
 
 for (i = 1; i < argc; i++)
 {
