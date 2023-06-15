f1aaaf63aac756497960627dcb8afe6f32f2b5c1
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-15 06:48:12 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,36 +1,89 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - entry point
- * @argc: number of arguments
- * @argv: array of arguments
+ * multiply - Multiplies two positive numbers
+ * @num1: First number as a string
+ * @num2: Second number as a string
  *
+ * Description: Multiplies two positive numbers and prints the result
+ */
+void multiply(char *num1, char *num2)
+{
+int len1 = 0, len2 = 0, i, j;
+int *result;
+
+
+while (num1[len1])
+len1++;
+while (num2[len2])
+len2++;
+
+result = calloc(len1 + len2, sizeof(int));
+
+
+for (i = len1 - 1; i >= 0; i--)
+{
+for (j = len2 - 1; j >= 0; j--)
+{
+int digit1 = num1[i] - '0';
+int digit2 = num2[j] - '0';
+int product = digit1 * digit2;
+
+int sum = result[i + j + 1] + product;
+result[i + j + 1] = sum % 10;
+result[i + j] += sum / 10;
+}
+}
+
+
+i = 0;
+while (result[i] == 0 && i < len1 + len2 - 1)
+i++;
+
+
+for (; i < len1 + len2; i++)
+printf("%d", result[i]);
+printf("\n");
+
+
+free(result);
+}
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Description: Program entry point, multiplies two positive numbers
  * Return: 0 on success, 98 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    char *result;
+int i, j;
 
-    /* check if the number of arguments is correct */
-    if (argc != 3)
-        _error();
 
-    /* check if the arguments are valid numbers */
-    if (!_isnumber(argv[1]) || !_isnumber(argv[2]))
-        _error();
+if (argc != 3)
+{
+printf("Error\n");
+return 98;
+}
 
-    /* perform long multiplication and store the result */
-    result = _long_mul(argv[1], argv[2]);
 
-    /* print the result followed by a new line */
-    printf("%s\n", result);
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return 98;
+}
+}
+}
 
-    /* free the memory allocated for the result */
-    free(result);
+multiply(argv[1], argv[2]);
 
-    /* return success */
-    return (0);
+return 0;
 }
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,17 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _ctoi(char c);
+char _itoc(int n);
+void _reverse(char *s);
+char *_add(char *s1, char *s2);
+char *_sub(char *s1, char *s2);
+char *_shift(char *s, int n);
+char *_trim(char *s);
+char **_split(char *s);
+char *_karatsuba_mul(char *s1, char *s2);
+int _isnumber(char *s);
+void _error(void);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
