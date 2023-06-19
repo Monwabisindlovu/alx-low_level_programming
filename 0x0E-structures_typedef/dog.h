0e2903869793d5ca4ee5150e7b0dd83c66bed0c6
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-19 00:50:27 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,16 @@
  * @age: Age of the dog
  * @owner: Owner of the dog
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
