444802ce636cb43d9387b20deff10afec8f8ebb2
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-19 01:07:28 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,9 +19,15 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+void print_dog(dog_t *d);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+void init_dog(dog_t *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(dog_t *d);
+
 #endif
 
 
