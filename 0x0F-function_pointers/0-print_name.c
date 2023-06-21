29e20343aa2d3bf37bc025a332b201126f418259
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-21 08:17:54 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
-#include <ctype.h>
-#include "function_pointers.h"
 
-/* function that prints a name as it is */
-void print_name_as_is(char *name)
-{
-    printf("%s\n", name);
-}
+/**
+ * print_name - Print name using function pointer.
+ * @name: Pointer to a character array representing the name.
+ * @f: Pointer to a function that takes a character pointer as an argument.
+ *
+ * Return: None
+ */
+void print_name(char *name, void (*f)(char *)) {
+if (name == NULL || f == NULL)
+return;
 
-/* function that prints a name in uppercase */
-void print_name_uppercase(char *name)
-{
-    int i;
-
-    for (i = 0; name[i] != '\0'; i++)
-    {name[i] = toupper(name[i]);
-}
-printf("%s\n", name);
-}
-
-/* function that prints a name using a function pointer */
-void print_name(char *name, void (*f)(char *))
-{
-if (name && f) /* check if the arguments are not NULL */
-{
-f(name); /* call the function pointed by f with name as argument */
-}
+f(name);
 }
 
