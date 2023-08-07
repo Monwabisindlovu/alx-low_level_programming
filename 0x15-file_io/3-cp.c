f5516801e4ce1efa580b7172165e2e7cc8c02dad
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-08-07 03:33:35 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,53 +1,84 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void copy_file(const char *file_from, const char *file_to);
 
 /**
- * main - Copies the content of a file to another file
+ * copy_file - Copies the content of a file to another file.
+ * @file_from: The name of the source file.
+ * @file_to: The name of the destination file.
  *
- * @argc: The number of arguments passed to the program
- * @argv: An array of strings containing the arguments
- *
- * Return: 0 on success, or the appropriate error code on failure
+ * Description: If the number of arguments is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
-int main(int argc, char **argv)
+void copy_file(const char *file_from, const char *file_to)
 {
-	FILE *file_from, *file_to;
-	int c;
+	int from, to, r, w;
+	char buffer[1024];
 
-	if (argc != 3)
+	from = open(file_from, O_RDONLY);
+	if (from == -1)
 	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	file_from = fopen(argv[1], "r");
-	if (file_from == NULL)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	file_to = fopen(argv[2], "w");
-	if (file_to == NULL)
+	to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (to == -1)
 	{
-		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-		fclose(file_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(from);
 		exit(99);
 	}
 
-	while ((c = fgetc(file_from)) != EOF)
+
+	while ((r = read(from, buffer, sizeof(buffer))) > 0)
 	{
-		if (fputc(c, file_to) == EOF)
+		w = write(to, buffer, r);
+		if (w == -1)
 		{
-			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-			fclose(file_from);
-			fclose(file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close(from);
+			close(to);
 			exit(99);
 		}
 	}
 
-	fclose(file_from);
-	fclose(file_to);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(from);
+		close(to);
+		exit(98);
+	}
+
+	if (close(from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		exit(100);
+	}
+
+	if (close(to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
+		exit(100);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file(argv[1], argv[2]);
 
 	return (0);
 }
