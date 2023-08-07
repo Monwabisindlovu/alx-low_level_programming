c2a8ca7950a4290f4eeb9a64da7a9166e397328b
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-08-07 03:37:42 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -69,7 +69,18 @@ void copy_file(const char *file_from, const char *file_to)
 		exit(100);
 	}
 }
-
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
