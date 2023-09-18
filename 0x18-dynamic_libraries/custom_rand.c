67be938f76a5b043bd9dd4a84e38d537f2c63d09
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-09-18 05:59:09 -0700

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/custom_rand.c
@@ -0,0 +1,12 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * rand - returns a fixed value to win the jackpot
+ *
+ * Return: 6, always
+ */
+int rand(void)
+{
+return (8);
+}
