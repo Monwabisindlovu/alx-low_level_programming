0950d59f4d92cbad2979878b4ed1d480dd308285
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-10-06 06:00:41 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,32 +10,29 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
-    unsigned long int i;
+hash_table_t *new_table;
+unsigned long int i;
 
-    if (size == 0)
-        return (NULL);
+if (size == 0)
+return (NULL);
 
-    /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
 
-    /* Allocate memory for the array of pointers to linked lists */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
+new_table->array = malloc(sizeof(hash_node_t *) * size);
+if (new_table->array == NULL)
+{
+free(new_table);
+return (NULL);
+}
 
-    new_table->size = size;
+new_table->size = size;
 
-    /* Initialize each array element to NULL */
-    for (i = 0; i < size; i++)
-    {
-        new_table->array[i] = NULL;
-    }
+for (i = 0; i < size; i++)
+{
+new_table->array[i] = NULL;
+}
 
 return (new_table);
 }
