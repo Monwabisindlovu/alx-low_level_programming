4ce0f28e7a80ba311371fe69aad98598ae50be0a
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-15 21:36:10 -0800

    0-linear

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,24 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-int linear_search(int *array, size_t size, int value);
+#include <math.h>
+#include <stddef.h>
 
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+void print_array(const int *array, size_t left, size_t right);
 #endif /* SEARCH_ALGOS_H */
 
